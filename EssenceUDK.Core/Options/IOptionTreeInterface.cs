d7e5a96e4dbe273e64c63158a7b454cbd7e3a33a
Xen85 <xen1985@gmail.com>
2015-11-15 18:04:52 +0100

    plugin Interfaces

---
+++ EssenceUDK.Core/Options/IOptionTreeInterface.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace EssenceUDK.Core.Options
+{
+
+    public interface IOptionTreeInterface
+    {
+        string Header { get; }
+        List<IOptionTreeInterface> List { get; }
+        
+    }
+
+    public interface IOptionTreeInterface<out T> : IOptionTreeInterface
+    {
+        T ViewModel { get; }
+    }
+
+
+}
---
+++ EssenceUDK.Core/Options/ITreeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace EssenceUDK.Core.Options
+{
+
+    public interface ITreeViewModel
+    {
+         IList<IOptionTreeInterface> List { get; }
+         void Populate();
+    }
+
+
+}
